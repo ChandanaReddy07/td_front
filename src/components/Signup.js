c00c7c5dc91bb71564940a65a786c7301cf53a8d
ChandanaReddy07 <ironchandana14@gmail.com>
2023-12-22 02:21:22 +0530

    updated

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -9,7 +9,7 @@ function Signup({ onSignupSuccess, onClose }) {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://todo-backend-nkpr.onrender.com//signup", {
+      const response = await axios.post("https://todo-backend-nkpr.onrender.com/signup", {
         name,
         email,
         password,
